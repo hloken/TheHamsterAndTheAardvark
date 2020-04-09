a7f8e4b4b0e8c29c4579645cae299cda9e86042f
Hans Løken <hans@ebamse.net>
2020-04-09 14:04:47 +0200

    Created solution, project for API endpoint and project for contracts shared between server and client

---
+++ PlanetEscapeServer/Api/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+
+    }
+}
---
+++ PlanetEscapeServer/PlanetEscape.Contracts/Dtos/Planet.cs
@@ -0,0 +1,7 @@
+﻿namespace Contracts.Dtos
+{
+    public class Planet
+    {
+        
+    }
+}
